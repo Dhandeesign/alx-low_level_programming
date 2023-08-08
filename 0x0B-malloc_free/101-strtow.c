bf51a46460eaf70558383318b4828bf862d6c278
Dhandeesign <d.aigbomian@gmail.com>
2023-08-08 07:05:15 -0700

    C - malloc, free (101-strtow.c update)

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,28 +4,27 @@
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
- *
  * Return: number of words
  */
 int count_word(char *s)
 {
-	int flag, b, a;
+	int flag, c, w;
 
 	flag = 0;
-	a = 0;
+	w = 0;
 
-	for (b = 0; s[b] != '\0'; b++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[b] == ' ')
+		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
 			flag = 1;
-			b++;
+			w++;
 		}
 	}
 
-	return (a);
+	return (w);
 }
 /**
  * **strtow - splits a string into words
@@ -36,10 +35,10 @@ int count_word(char *s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int q, k = 0, fil = 0, words, s = 0, start, end;
+	int q, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + fil))
-		fil++;
+	while (*(str + len))
+		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
@@ -48,25 +47,25 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (q = 0; q <= fil; q++)
+	for (q = 0; q <= len; q++)
 	{
 		if (str[q] == ' ' || str[q] == '\0')
 		{
-			if (s)
+			if (c)
 			{
 				end = q;
-				tmp = (char *) malloc(sizeof(char) * (s + 1));
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-				matrix[k] = tmp - s;
+				matrix[k] = tmp - c;
 				k++;
-				s = 0;
+				c = 0;
 			}
 		}
-		else if (s++ == 0)
+		else if (c++ == 0)
 			start = q;
 	}
 
